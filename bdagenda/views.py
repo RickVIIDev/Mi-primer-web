3747da8f51ef83e2623867e85c61682efa5ae39e
RickVIIDev <riloje07@gmail.com>
2023-01-22 10:38:13 -0800

    Templates modificados para mostrar contacto desde base de datos.

--- bdagenda/views.py
+++ bdagenda/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Contacto #el punto antes de models indica que estan en el directorio actual Esto significa que podemos utilizar "." y el nombre del archivo (sin .py). 
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'bdagenda/post_list.html', {})
+	contactos = Contacto.objects.all()
+	return render(request, 'bdagenda/post_list.html', {'contactos':contactos})
